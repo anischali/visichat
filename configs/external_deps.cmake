06368a11ac59b59659483d6495d335ab5dd43042
Anis CHALI <anis.chali1@outlook.com>
2024-11-24 06:23:42 -0500

    configs: add lite-p2p

--- configs/external_deps.cmake
+++ configs/external_deps.cmake
@@ -1,10 +1,16 @@
 include(FetchContent)
 
 FetchContent_Declare(Qbackend
-  GIT_REPOSITORY https://github.com/anischali/Qbackend.git
-  GIT_TAG main
+    GIT_REPOSITORY https://github.com/anischali/Qbackend.git
+    GIT_TAG main
 )
 FetchContent_MakeAvailable(Qbackend)
+
+FetchContent_Declare(lite-p2p
+    GIT_REPOSITORY https://github.com/anischali/lite-p2p.git
+    GIT_TAG main
+)
+FetchContent_MakeAvailable(lite-p2p)
 
 FetchContent_Declare(fmt
   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
@@ -17,3 +23,14 @@ FetchContent_Declare(nlohmann_json
   GIT_TAG v3.11.2
 )
 FetchContent_MakeAvailable(nlohmann_json)
+
+if (ANDROID)
+    include(FetchContent)
+    FetchContent_Declare(
+        android_openssl
+        DOWNLOAD_EXTRACT_TIMESTAMP true
+        URL https://github.com/KDAB/android_openssl/archive/refs/heads/master.zip
+    )
+    FetchContent_MakeAvailable(android_openssl)
+    include(${android_openssl_SOURCE_DIR}/android_openssl.cmake)
+endif()
